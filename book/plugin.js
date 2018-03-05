aee20bfe895251aedbd51a38768bafa4ede72538
Sam Lau <samlau95@gmail.com>
2018-03-05 00:09:18 -0800

    Allow $ for inline math

--- book/plugin.js
+++ book/plugin.js
@@ -1,11 +1,16 @@
-require(["gitbook"], function(gitbook) {
-    MathJax.Hub.Config({
-        tex2jax: {}
-    });
+require(['gitbook'], function(gitbook) {
+  MathJax.Hub.Config({
+    tex2jax: {
+      inlineMath: [['$', '$'], ['\\\\(', '\\\\)']],
+      processEscapes: true,
 
+      CommonHTML: { linebreaks: { automatic: true } },
+      'HTML-CSS': { linebreaks: { automatic: true } },
+      SVG: { linebreaks: { automatic: true } },
+    },
+  })
 
-    gitbook.events.bind("page.change", function() {
-        MathJax.Hub.Typeset()
-    });
-});
-
+  gitbook.events.bind('page.change', function() {
+    MathJax.Hub.Typeset()
+  })
+})
